2f003a2d65cde61fc1a5fb557adf95019e297695
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-04 15:22:40 -0800

    prepared git files for deployment to heroku

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,18 +4,18 @@
   reposObj.allRepos = [];
 
   reposObj.requestRepos = function(callback) {
-    $.get('/github/users/codefellows-portland-301d7/repos' +
+    $.get('/github/user/repos' + //users/repos is the authenticated version
           '?per_page=10&sort=updated')
           .done(function(data) {
             reposObj.allRepos = data;
-            $.get('/github/users/browniefed/followers') // second ajax call
+            $.get('/github/user/followers') // second ajax call
             .done(function(data) {
               reposObj.followers = data;
             })
             .done(callback); // the callback will be the renderRepos
           });
   };
-  
+
   reposObj.withTheAttribute = function(attr) {
     return reposObj.allRepos.filter(function(aRepo) {
       return aRepo[attr];
